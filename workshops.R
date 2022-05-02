524e8b8ee276f5129972cf0397e19f77ce761374
mohammad <moaziz.me@outlook.com>
2022-05-02 18:14:13 +1000

    workshopsession2

---
+++ workshops.R
@@ -0,0 +1,15 @@
+##Week1session2workshop
+
+bird1<-read.csv("birds.csv") #upload file - make sure file is in same dir to the Rprogram-file
+View(bird1) #view the data set to make a blue print of it in you current project
+
+sex<-bird1$sex #assign sex column to a seperate value table "sex"
+curlen<-bird1$curlen   #assign curlen column to a seperate value table "curlen"
+sexcode<-ifelse(sex=="F", 1, 0) #code famale to 1 and male to 0 and give it 'sexcode' name
+plot(curlen, jitter(sexcode, 0.15), pch=19, xlab="Bill length(mm)", ylab = "sex(0 - male, 1 - female)")
+#plot "CURLEN" use "JITTER PLOT" plot sex code 0.15 show how disterbuted they are
+#pch19 is the type of shape you want to us, then xy labs name.
+model <- glm(sexcode~curlen, binomial()) #apply glm model to sexcode&curlen
+xv<-seq(min(curlen), max(curlen), 0.01) #use on your regression model 'Curlen as min'
+yv<-predict(model, list(curlen=xv), type="response") #predict your model
+lines(xv, yv, col="green") #create a line
