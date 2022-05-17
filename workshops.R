6e47098b7b14909fc3714bc73e69627c522e7318
Moaziz-me <moaziz.me@outlook.com>
2022-05-18 09:20:28 +1000

    week4workshop7

--- workshops.R
+++ workshops.R
@@ -39,3 +39,99 @@ diamonds%>%
   summarise(avg_price=mean(price))
 
 #week2Workshop4
+install.packages("tidymodels", "dplyr", "recipes", "rsample", "modeldata")
+
+library(recipes)
+library(rsample)
+library(modeldata)
+
+data("credit_data")
+
+set.seed(55)
+train_test_split <- initial_split(credit_data)
+
+credit_train <- training(train_test_split)
+credit_test <- testing(train_test_split)
+
+
+#week3workshop5
+install.packages("MASS")
+install.packages("ISLR")
+install.packages("readxl")
+library(MASS)
+library(ISLR)
+library(readxl)
+
+#age<-read_excel('age.xls', sheet='Hoja2')
+age<-read_excel('age.xls')
+
+lmheight=lm(height~age, data=age)
+summary(lmheight)
+
+lmheight2 <- lm(height~age, data=age)
+summary(lmheight2)
+
+pressure <- read_excel('pressure.xlsx')
+lmTemp<-lm(Pressure~Temperature, data = pressure)
+plot(pressure, pch=17, col='blue')
+abline(lmTemp)
+
+item<-read_excel('item.xlsx')
+lmItem<-lm(id~cost, data = item)
+plot(item, pch=15, col="red")
+abline(lmItem)
+
+
+#bagging and boosting?
+
+linear_reg(item)%>%
+  set_engine()%>%
+  translate()
+
+?fit()
+fit_xy()
+
+##inclass activity:
+#lda MASS predict(obj)
+#glm stats predict(obj, type = "response")
+#gbm bgm predict(obj, type = "response", n.trees)
+#mda mda predict(obj, type = "response")
+#rpart rpart predict(obj, type = "prob")
+#weka Rweka predict(obj, type = "probability")
+
+library(tidymodels)
+reg_model <- linear_reg(penalty = 0.01)
+reg_model
+
+reg_model %>%
+  set_engine("glmnet")%>%
+  translate()
+
+install.packages("glmnet")
+library(glmnet)
+
+#reg_model %>%
+#  set_engine("glmnet")%>%
+#  fit(price ~ ., data = item)
+
+reg_model %>%
+  set_engine("glmnet")%>%
+  fit(mpg ~ ., data = mtcars)
+
+reg_model %>%
+  set_engine("glmnet")%>%
+  fit(mpg ~ ., data = mtcars %>% slice(1:29))%>%
+  predict(new_data = mtcars %>% slice(30:32))
+
+reg_model %>%
+  set_engine("glmnet")%>%
+  fit_xy(x = mtcars %>% select(-mpg),
+         y =mtcars$mpg)
+view(mtcars)
+
+#week2workshop6
+
+install.packages("xlsx")
+library(xlsx)
+
+data<-read_excel('commerce.xlsx')
